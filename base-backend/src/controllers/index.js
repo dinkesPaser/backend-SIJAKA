d1a18156ab815ed599fa6c267e0209a3495f305f
imamahmadf <fahruraziimam22@gmail.comm>
2025-05-19 15:45:42 +0800

    fix pescription

--- base-backend/src/controllers/index.js
+++ base-backend/src/controllers/index.js
@@ -1,33 +1,5 @@
-const perjalananControllers = require("./perjalananControllers");
-const pegawaiControllers = require("./pegawaiControllers");
-const kwitansiControllers = require("./kwitansiControllers");
-const dalamKotaControllers = require("./dalamKotaControllers");
-const rillControllers = require("./rillControllers");
 const userControllers = require("./userControllers");
-const templateControllers = require("./templateControllers");
-const adminControllers = require("./adminControllers");
-const klasifikasiControllers = require("./klasifikasiControllers");
-const tujuanControllers = require("./tujuanControllers");
-const indukUnitKerjaControllers = require("./indukUnitKerjaControllers");
-const pajakControllers = require("./pajakControllers");
-const keuanganControllers = require("./keuanganControllers");
-const nomorSuratControllers = require("./nomorSuratControllers");
-const subKegiatanControllers = require("./subKegiatanControllers");
 
 module.exports = {
-  perjalananControllers,
-  pegawaiControllers,
-  kwitansiControllers,
-  dalamKotaControllers,
-  rillControllers,
   userControllers,
-  templateControllers,
-  adminControllers,
-  klasifikasiControllers,
-  tujuanControllers,
-  indukUnitKerjaControllers,
-  pajakControllers,
-  keuanganControllers,
-  nomorSuratControllers,
-  subKegiatanControllers,
 };
